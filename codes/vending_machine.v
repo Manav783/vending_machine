1f3d019b43b09cb4ba9de0a00e581f05e968d899
Manav <iammanav104@gmail.com>
2026-01-03 19:02:44 +0530

    Create vending_machine.v

---
+++ codes/vending_machine.v
@@ -0,0 +1,39 @@
+module vending_machine(
+  input clk, rst, buy, load,
+  input [3:0] b_in,
+  input [1:0] sel,
+  output [6:0] seg,
+  output [3:0] an,
+  output yes,no
+);
+
+	wire [3:0] balance, tens, ones;
+  	main u_main (
+		  .clk(clk),
+		  .rst(rst),
+		  .buy(buy),
+		  .load(load),
+		  .b_in(b_in),
+		  .sel(sel),
+		  .b_out(balance),
+		  .yes(yes),
+		  .no(no)
+		);
+
+  
+    bin_to_bcd_4bit bcd ( 
+      .bin(balance),
+    	.tens(tens),
+    	.ones(ones)
+    );
+  
+    seven_seg_driver disp(
+		  .clk(clk),
+		  .tens(tens),
+		  .ones(ones),
+		  .seg(seg),
+		  .an(an)
+		);
+
+  
+endmodule
